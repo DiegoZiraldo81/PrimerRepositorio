81ef92c58d137d8f2e8380297b6b9e0d5ddcf3c2
Diego Ziral <diegoziraldo@gmail.com>
2023-08-25 16:13:26 -0300

    no tengo idea de que estoy haciendo

---
+++ primerArchivo.py
@@ -0,0 +1,6 @@
+print("no tengo idea de que estoy haciendo")
+## Abri github
+## Instalé GIT
+## Instalé GITHUB Desktop
+## Instalé Visual Studio Code
+## Creé un repositorio en GITHUB Desktop y le guarde un archivo dentro
